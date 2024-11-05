169d4b569de725b5ebb74419b01bbf87544811dc
Ak1_AleX <2006akimov@gmai.com>
2024-11-05 03:54:49 +0300

    made the feature of reading arcane desc from the file and showing it after solving arcane number

--- etu_Akimov/test.c
+++ etu_Akimov/test.c
@@ -1,9 +1,33 @@
-#include <stdio.h>
-
-int main() {
-	FILE* arcaneDesc = fopen("log.txt", "r");
-	int a = 10;
-	fprintf(arcaneDesc, "smth\n");
-
-	fclose(arcaneDesc);
-}
+//#include <stdio.h>
+//#include <locale.h>
+//
+//int main() {
+//	setlocale(LC_ALL, "Ru");
+//
+//	FILE* arcaneDesc = fopen("log.txt", "r");
+//	char arcaneLine[1000];
+//	memset(arcaneLine, ' ', sizeof(arcaneLine));
+//	fgets(arcaneLine, 1000, arcaneDesc);
+//	for (int i = 0; i <= 100; i++) {
+//		printf("%c", arcaneLine[i]);
+//	}
+//	printf("\n");
+//	for (int i = 101; i <= 200; i++) {
+//		printf("%c", arcaneLine[i]);
+//	}
+//	printf("\n");
+//	for (int i = 201; i <= 300; i++) {
+//		printf("%c", arcaneLine[i]);
+//	}
+//	printf("\n");
+//	for (int i = 301; i <= 400; i++) {
+//		printf("%c", arcaneLine[i]);
+//	}
+//	printf("\n");
+//	for (int i = 401; i <= 500; i++) {
+//		printf("%c", arcaneLine[i]);
+//	}
+//	printf("\n");
+//
+//	fclose(arcaneDesc);
+//}
